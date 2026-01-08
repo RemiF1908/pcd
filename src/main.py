8f8cf331f0aca63db85043d1f8a0fc42d625743b
RemiF <remi.ferrato@gmail.com>
2026-01-08 21:00:30 +0100

    feat: add entrypoint

--- src/main.py
+++ src/main.py
@@ -1,14 +1,97 @@
-from src.model.dungeon import Dungeon
-from src.model.level import Level
-from simulation import Simulation
+import argparse
+from src.model.level import LevelBuilder
+from src.simulation import Simulation
 from src.controller.game_controller import GameController
+from src.model.hero import Hero
+from src.model.dungeon import Dungeon
+from src.model.cell import Cell
+from src.model.floor import Floor
+from src.model.wall import Wall
+from src.model.trap import Trap
+from src.view.tui.simulation_display import TUIView, ColorPair
+from src.view.gui.gui import GuiView
+from src.commands.GameInvoker import GameInvoker
 
-donjon = Dungeon(dimension=(10, 10), grid=[], entry=(0, 0), exit=(9, 9))
-level = Level(dungeon=donjon, budget_tot=200, difficulty=3)
+def main():
+    parser = argparse.ArgumentParser(description="Dungeon Manager")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--tui", action="store_true", help="Run the terminal interface")
+    group.add_argument("--web", action="store_true", help="Run the web interface")
+    args = parser.parse_args()
+    print(args)
 
-simu = Simulation(dungeon=donjon, budget_tot=level.budget_tot, nb_heroes=level.nb_heroes, heroes=level.heroes)
 
-gc = GameController(interface=0, simulation=simu)
+    gamecontroller = GameController(None, None)
+    invoker = GameInvoker(gamecontroller)
+
+    # Création d'un niveau de test
+    hero = Hero(pv_total=100, strategy="safest")
+    hero2 = Hero(pv_total=100, strategy="shortest", hero_number=2)
+    rows, cols = 10, 10
+    dungeon = Dungeon(
+        dimension=(rows, cols),
+        grid=[[Cell((r, c), Floor()) for c in range(cols)] for r in range(rows)],
+        entry=(0, 0),
+        exit=(rows - 1, cols - 1),
+    )
+
+    level = (
+        LevelBuilder()
+        .set_dungeon(dungeon=dungeon)
+        .set_difficulty(4)
+        .set_budget(2000)
+        .add_hero_instance(hero)
+        .add_hero_instance(hero2)
+        .build()
+    )
+
+    # Création de la simulation
+    simulation = Simulation(level, level.dungeon)
+    gamecontroller.simulation = simulation
+
+    view = TUIView(
+        status_info=gamecontroller.get_status_info(),
+        dimension=gamecontroller.dimension(),
+        entry_pos=gamecontroller.entry(),
+        exit_pos=gamecontroller.exit(),
+        heros_positions=gamecontroller.get_hero_positions(),
+        invoker=invoker,
+        dungeon=gamecontroller.dungeon,
+        simulation=gamecontroller.simulation,
+    )
+
+    
 
 
+    if args.tui:
+        
+        view = TUIView(
+            status_info=gamecontroller.get_status_info(),
+            dimension=gamecontroller.dimension(),
+            entry_pos=gamecontroller.entry(),
+            exit_pos=gamecontroller.exit(),
+            heros_positions=gamecontroller.get_hero_positions(),
+            invoker=invoker,
+            dungeon=gamecontroller.dungeon,
+            simulation=gamecontroller.simulation,
+        )
 
+    elif args.web:
+        view = GuiView(
+            dimension=gamecontroller.dimension(),
+            entry_pos=gamecontroller.entry(),
+            exit_pos=gamecontroller.exit(),
+            heros_positions=gamecontroller.get_hero_positions(),
+            invoker=invoker,
+            dungeon=gamecontroller.dungeon,
+            simulation=gamecontroller.simulation,
+        )
+
+    print("ee")
+    gamecontroller.interface = view
+
+    view.run()
+
+
+if __name__ == "__main__":
+    main()
